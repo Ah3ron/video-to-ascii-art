2aff8c0d081c72b867cb2c1e9b44e13a2947360e
Ah3ron <ah3ron@protonmail.com>
2024-04-17 19:09:53 +0300

    Code optimised

--- main.js
+++ main.js
@@ -2,96 +2,62 @@ import './style.css'
 import video_url from "/pretty_cvnt.mp4"
 
 document.querySelector('#app').innerHTML = `
-<div id="output"></div>
-<video id="input" autoplay muted loop playsinline crossorigin="anonymous">
-</video>
-<canvas id="prerender" width="158" height="43"></canvas>
+  <div id="output"></div>
+  <video id="input" autoplay muted loop playsinline crossorigin="anonymous"></video>
+  <canvas id="prerender" width="144" height="39"></canvas>
 `;
 
 const video = document.getElementById("input");
-const canvas = document.getElementById("prerender");
 const output = document.getElementById("output");
-const ctx = canvas.getContext("2d", { willReadFrequently: true });
-const charsFixed = [
-  ['.', '-'],
-  [':', '_'],
-  ["'", ':'],
-  [',', '^'],
-  ['=', ';'],
-  ['>', '<'],
-  ['+', '!'],
-  ['r', 'c'],
-  ['*', '/'],
-  ['z', '?'],
-  ['s', 'L'],
-  ['T', 'v'],
-  [')', 'J'],
-  ['7', '|'],
-  ['(', '|'],
-  ['F', 'i'],
-  ['{', 'C'],
-  ['}', 'f'],
-  ['I', '3'],
-  ['1', 't'],
-  ['l', 'u'],
-  ['[', 'n'],
-  ['e', 'o'],
-  ['Z', '5'],
-  ['Y', 'x'],
-  ['j', 'y'],
-  ['a', ']'],
-  ['2', 'E'],
-  ['S', 'w'],
-  ['q', 'k'],
-  ['P', '6'],
-  ['h', '9'],
-  ['d', '4'],
-  ['V', 'p'],
-  ['O', 'G'],
-  ['b', 'U'],
-  ['A', 'K'],
-  ['X', 'H'],
-  ['m', '8'],
-  ['R', 'D'],
-  ['#', '$'],
-  ['B', 'g'],
-  ['0', 'M'],
-  ['N', 'W'],
-  ['Q', '%'],
+
+const canvas = document.getElementById("prerender");
+const ctx = canvas.getContext("2d", { willReadFrequently: true, alpha: false });
+
+const MAX_COLOR_INDEX = 255;
+const chars = [
+  ['.', '-'], [':', '_'], ["'", ':'],
+  [',', '^'], ['=', ';'], ['>', '<'],
+  ['+', '!'], ['r', 'c'], ['*', '/'],
+  ['z', '?'], ['s', 'L'], ['T', 'v'],
+  [')', 'J'], ['7', '|'], ['(', '|'],
+  ['F', 'i'], ['{', 'C'], ['}', 'f'],
+  ['I', '3'], ['1', 't'], ['l', 'u'],
+  ['[', 'n'], ['e', 'o'], ['Z', '5'],
+  ['Y', 'x'], ['j', 'y'], ['a', ']'],
+  ['2', 'E'], ['S', 'w'], ['q', 'k'],
+  ['P', '6'], ['h', '9'], ['d', '4'],
+  ['V', 'p'], ['O', 'G'], ['b', 'U'],
+  ['A', 'K'], ['X', 'H'], ['m', '8'],
+  ['R', 'D'], ['#', '$'], ['B', 'g'],
+  ['0', 'M'], ['N', 'W'], ['Q', '%'],
   ['&', '@']
 ];
-let chars = [...charsFixed];
 let charsLength = chars.length;
-const MAX_COLOR_INDEX = 255;
+const w = canvas.width;
+const h = canvas.height;
 
 const updateCanvas = () => {
-  const w = canvas.width;
-  const h = canvas.height;
   ctx.drawImage(video, 0, 0, w, h);
   const data = ctx.getImageData(0, 0, w, h).data;
-  let outputText = "";
+  let outputTextArr = Array(h);
   for (let y = 0; y < h; y++) {
-    let row = "";
+    let rowArr = Array(w);
     for (let x = 0; x < w; x++) {
       const index = (x + y * w) * 4;
       const [r, g, b] = data.slice(index, index + 3);
       const c = (r + g + b) / 3;
-      const charIndex = Math.floor(
-        (charsLength * ((c * 100) / MAX_COLOR_INDEX)) / 100
-      );
+      const charIndex = Math.floor(charsLength * (c / MAX_COLOR_INDEX));
       const result = chars[charIndex];
       const char = Array.isArray(result)
-        ? result[Math.floor(Math.random() * result.length) + 0]
+        ? result[Math.floor(Math.random() * result.length)]
         : result;
 
-      row += `<span style="color: rgb(${r}, ${g}, ${b});">${char ?? "&nbsp;"
-        }</span>`;
+      rowArr[x] = `<span style="color: rgb(${r}, ${g}, ${b});">${char ?? "&nbsp;"}</span>`;
     }
-    outputText += `<div>${row}</div>`;
+    outputTextArr[y] = `<div>${rowArr.join('')}</div>`;
   }
-  output.innerHTML = outputText;
-
-  setTimeout(() => requestAnimationFrame(updateCanvas), 0);
+  output.innerHTML = outputTextArr.join('');
+  requestAnimationFrame(updateCanvas);
 };
 
 requestAnimationFrame(() => {
@@ -100,17 +66,12 @@ requestAnimationFrame(() => {
 });
 
 const config = {
-  speed: 1,
-  zoom: 18,
+  zoom: 20,
   video: video_url,
-  isolation: 0,
-  brightness: 1.5,
+  brightness: 1.2,
   play: true
 };
 
-const updateSpeed = (value) => {
-  video.playbackRate = value;
-};
 
 const updateBrightness = (value) => {
   output.style.filter = `brightness(${value})`;
@@ -118,11 +79,6 @@ const updateBrightness = (value) => {
 
 const updateZoom = (value) => {
   document.documentElement.style.fontSize = `${value}px`;
-};
-
-const updateIsolation = (value) => {
-  chars = [...new Array(value).fill("&nbsp;"), ...charsFixed];
-  charsLength = chars.length;
 };
 
 const updateVideo = (value) => {
@@ -136,9 +92,7 @@ const updatePlay = (value) => {
   value ? video.play() : video.pause();
 };
 
-updateSpeed(config.speed);
 updateBrightness(config.brightness);
 updateZoom(config.zoom);
-updateIsolation(config.isolation);
 updateVideo(config.video);
 updatePlay(config.play);
